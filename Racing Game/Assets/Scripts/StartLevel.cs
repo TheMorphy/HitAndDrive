ac428a2aff0a3b53a95f7a134513e17771e19114
dabbopoker <dabidesantos@gmail.com>
2022-03-20 00:13:41 +0000

    fixes

--- Racing Game/Assets/Scripts/StartLevel.cs
+++ Racing Game/Assets/Scripts/StartLevel.cs
@@ -5,18 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class StartLevel : MonoBehaviour
 {
-    [SerializeField] GameObject car;
     [SerializeField] private string newGameLevel;
-
-    CarController carScript;
-    LevelSystem levelSystem;
 
     string levelToLoad;
 
     private void Start()
     {
-        carScript = car.GetComponent<CarController>();
-        levelSystem = GetComponent<LevelSystem>();
         levelToLoad = PlayerPrefs.GetString("LevelSaved");
 
         if (PlayerPrefs.HasKey("LevelSaved"))
